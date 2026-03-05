6aad4fc16af4ee2588dda878234e12b102944001
Pratik Markad <pratik.markad1@gmail.com>
2026-03-05 19:57:45 +0530

    initial commit: add basic Spring Boot project with required dependencies

---
+++ server/hms-room-service/src/main/java/com/hms/room/HmsRoomServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hms.room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsRoomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsRoomServiceApplication.class, args);
+	}
+
+}
---
+++ server/hms-user-service/src/test/java/com/hms/user/service/HmsUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hms.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
